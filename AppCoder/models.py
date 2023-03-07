23f4733521b7aa41cab681f659d5e02d446ee434
SPascualetto <124947317+SPascualetto@users.noreply.github.com>
2023-03-07 13:45:22 -0300

    Add files via upload

---
+++ AppCoder/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Curso(models.Model):
+    
+    nombre= models.CharField(max_length=40)
+    camada= models.IntegerField()
+    
+class Estudiante(models.Model):
+    nombre= models.CharField(max_length=40)
+    apellido= models.CharField(max_length=40)
+    email= models.EmailField(max_length=30)
+
+class Profesor(models.Model):
+    nombre= models.CharField(max_length=40)
+    apellido= models.CharField(max_length=40)
+    email= models.EmailField(max_length=30)
+    profesion= models.CharField(max_length=30)
+
+class Entregable(models.Model):
+    nombre= models.CharField(max_length=40)
+    fechaDeEntrega= models.DateField()
+    email= models.BooleanField(max_length=30)
