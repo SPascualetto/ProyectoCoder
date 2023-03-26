79990ee6a53afc4256f49eb8ed24000b4084e763
SPascualetto <124947317+SPascualetto@users.noreply.github.com>
2023-03-26 16:23:15 -0300

    Delete AppCoder directory

--- AppCoder/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Alumnos, Profesores, Egresados, Institucional
-
-admin.site.register(Alumnos)
-admin.site.register(Profesores)
-admin.site.register(Egresados)
-admin.site.register(Institucional)
