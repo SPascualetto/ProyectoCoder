0e3524f1976620dd82b1837a4fd79a1fd68ea9e5
SPascualetto <124947317+SPascualetto@users.noreply.github.com>
2023-04-07 02:28:01 -0300

    Delete registro directory

--- registro/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegistroPagina, UsuarioEdicion
-
-urlpatterns = [
-    path('register/', RegistroPagina.as_view(),name='register'), 
-    path('editar/', UsuarioEdicion.as_view(), name='editar'),
-    
-    
-]
